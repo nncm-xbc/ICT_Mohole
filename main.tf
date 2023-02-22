f41c5835b0ec54cea0c599ec3e2229bddef6b478
nncm-xbc <simon.hugot.67@gmail.com>
2023-02-22 21:39:40 +0100

    Change of the instance name

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server1" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
